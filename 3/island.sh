47ac63fb72e2db8ce601f4c763b7d8ede3ea178d
Bruce Cherniak <Bruce.Cherniak@Intel.com>
2019-03-28 21:43:21 -0500

    Add linear_to_srgb conversion to handle denoised hdr image buffer.

--- 3/island.sh
+++ 3/island.sh
@@ -3,4 +3,4 @@ source runvars
 CAMERA="-sg:apertureRadius=0.003125 -sg:focusDistance=1675.338285 -sg:fovy=32.4138"
 CAMERA="${CAMERA} -vp -1139.015893 23.286733 1479.794723    -vi  244.814337 238.807148 560.380117    -vu -0 1 0"
 
-${OSP}  ${COMMON} /mnt/seenas2/data/moana/island/island.biff ${CAMERA}
+${OSP}  ${COMMON} ${BIFFS}/island.biff ${CAMERA} $@
